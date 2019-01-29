64f9dcd0b435b4e2321504b7609143d67879734b
Héctor De La Fuente <1630444@upv.edu.mx>
2019-01-28 22:26:44 -0600

    primer commit

---
+++ administrator/C:/xampp/htdocs/joomla-templates/jsn_force_2/administrator/logs/error.php
@@ -0,0 +1,1 @@
+2017-06-27T11:10:18+00:00	INFO 192.168.1.72	joomlafailure	Username and password do not match or you do not have an account yet.
---
+++ administrator/logs/com_virtuemart.log.php
@@ -0,0 +1,79 @@
+#
+#<?php die("Forbidden."); ?>
+
+2017-06-26 04:16:23 ERROR vmError: File not found C:\xampp\htdocs\joomla-templates\jsn_force_2/images/stories/virtuemart/product/151.jpg
+2017-06-26 04:16:23 ERROR vmError: Couldnt create thumb, file not found C:\xampp\htdocs\joomla-templates\jsn_force_2\images\stories\virtuemart\product\151.jpg
+2017-06-26 04:16:23 ERROR vmError: File not found C:\xampp\htdocs\joomla-templates\jsn_force_2/images/stories/virtuemart/product/693.jpg
+2017-06-26 04:16:23 ERROR vmError: Couldnt create thumb, file not found C:\xampp\htdocs\joomla-templates\jsn_force_2\images\stories\virtuemart\product\693.jpg
+2017-06-26 04:16:23 ERROR vmError: File not found C:\xampp\htdocs\joomla-templates\jsn_force_2/images/stories/virtuemart/product/57.jpg
+2017-06-26 04:16:23 ERROR vmError: Couldnt create thumb, file not found C:\xampp\htdocs\joomla-templates\jsn_force_2\images\stories\virtuemart\product\57.jpg
+2017-06-26 04:16:23 ERROR vmError: File not found C:\xampp\htdocs\joomla-templates\jsn_force_2/images/stories/virtuemart/product/424.jpg
+2017-06-26 04:16:23 ERROR vmError: Couldnt create thumb, file not found C:\xampp\htdocs\joomla-templates\jsn_force_2\images\stories\virtuemart\product\424.jpg
+2017-06-26 04:16:23 ERROR vmError: File not found C:\xampp\htdocs\joomla-templates\jsn_force_2/images/stories/virtuemart/product/3.jpg
+2017-06-26 04:16:23 ERROR vmError: Couldnt create thumb, file not found C:\xampp\htdocs\joomla-templates\jsn_force_2\images\stories\virtuemart\product\3.jpg
+2017-06-26 04:16:23 ERROR vmError: File not found C:\xampp\htdocs\joomla-templates\jsn_force_2/images/stories/virtuemart/product/397.jpg
+2017-06-26 04:16:23 ERROR vmError: Couldnt create thumb, file not found C:\xampp\htdocs\joomla-templates\jsn_force_2\images\stories\virtuemart\product\397.jpg
+2017-06-26 04:16:23 ERROR vmError: File not found C:\xampp\htdocs\joomla-templates\jsn_force_2/images/stories/virtuemart/product/151.jpg
+2017-06-26 04:16:23 ERROR vmError: Couldnt create thumb, file not found C:\xampp\htdocs\joomla-templates\jsn_force_2\images\stories\virtuemart\product\151.jpg
+2017-06-26 04:16:23 ERROR vmError: File not found C:\xampp\htdocs\joomla-templates\jsn_force_2/images/stories/virtuemart/product/28.jpg
+2017-06-26 04:16:23 ERROR vmError: Couldnt create thumb, file not found C:\xampp\htdocs\joomla-templates\jsn_force_2\images\stories\virtuemart\product\28.jpg
+2017-06-26 04:16:23 ERROR vmError: File not found C:\xampp\htdocs\joomla-templates\jsn_force_2/images/stories/virtuemart/product/397.jpg
+2017-06-26 04:16:23 ERROR vmError: Couldnt create thumb, file not found C:\xampp\htdocs\joomla-templates\jsn_force_2\images\stories\virtuemart\product\397.jpg
+2017-06-26 04:16:23 ERROR vmError: File not found C:\xampp\htdocs\joomla-templates\jsn_force_2/images/stories/virtuemart/product/424.jpg
+2017-06-26 04:16:23 ERROR vmError: Couldnt create thumb, file not found C:\xampp\htdocs\joomla-templates\jsn_force_2\images\stories\virtuemart\product\424.jpg
+2017-06-26 04:16:23 ERROR vmError: File not found C:\xampp\htdocs\joomla-templates\jsn_force_2/images/stories/virtuemart/product/39.jpg
+2017-06-26 04:16:23 ERROR vmError: Couldnt create thumb, file not found C:\xampp\htdocs\joomla-templates\jsn_force_2\images\stories\virtuemart\product\39.jpg
+2017-06-26 04:16:23 ERROR vmError: File not found C:\xampp\htdocs\joomla-templates\jsn_force_2/images/stories/virtuemart/product/37.jpg
+2017-06-26 04:16:23 ERROR vmError: Couldnt create thumb, file not found C:\xampp\htdocs\joomla-templates\jsn_force_2\images\stories\virtuemart\product\37.jpg
+2017-06-26 04:16:23 ERROR vmError: File not found C:\xampp\htdocs\joomla-templates\jsn_force_2/images/stories/virtuemart/product/58.jpg
+2017-06-26 04:16:23 ERROR vmError: Couldnt create thumb, file not found C:\xampp\htdocs\joomla-templates\jsn_force_2\images\stories\virtuemart\product\58.jpg
+2017-06-26 04:16:28 ERROR vmError: File not found C:\xampp\htdocs\joomla-templates\jsn_force_2/images/stories/virtuemart/product/151.jpg
+2017-06-26 04:16:28 ERROR vmError: Couldnt create thumb, file not found C:\xampp\htdocs\joomla-templates\jsn_force_2\images\stories\virtuemart\product\151.jpg
+2017-06-26 04:16:28 ERROR vmError: File not found C:\xampp\htdocs\joomla-templates\jsn_force_2/images/stories/virtuemart/product/397.jpg
+2017-06-26 04:16:28 ERROR vmError: Couldnt create thumb, file not found C:\xampp\htdocs\joomla-templates\jsn_force_2\images\stories\virtuemart\product\397.jpg
+2017-06-26 04:16:28 ERROR vmError: File not found C:\xampp\htdocs\joomla-templates\jsn_force_2/images/stories/virtuemart/product/3.jpg
+2017-06-26 04:16:28 ERROR vmError: Couldnt create thumb, file not found C:\xampp\htdocs\joomla-templates\jsn_force_2\images\stories\virtuemart\product\3.jpg
+2017-06-26 04:16:28 ERROR vmError: File not found C:\xampp\htdocs\joomla-templates\jsn_force_2/images/stories/virtuemart/product/28.jpg
+2017-06-26 04:16:28 ERROR vmError: Couldnt create thumb, file not found C:\xampp\htdocs\joomla-templates\jsn_force_2\images\stories\virtuemart\product\28.jpg
+2017-06-26 04:16:28 ERROR vmError: File not found C:\xampp\htdocs\joomla-templates\jsn_force_2/images/stories/virtuemart/product/424.jpg
+2017-06-26 04:16:28 ERROR vmError: Couldnt create thumb, file not found C:\xampp\htdocs\joomla-templates\jsn_force_2\images\stories\virtuemart\product\424.jpg
+2017-06-26 04:16:28 ERROR vmError: File not found C:\xampp\htdocs\joomla-templates\jsn_force_2/images/stories/virtuemart/product/693.jpg
+2017-06-26 04:16:28 ERROR vmError: Couldnt create thumb, file not found C:\xampp\htdocs\joomla-templates\jsn_force_2\images\stories\virtuemart\product\693.jpg
+2017-06-26 04:17:03 ERROR vmError: File not found C:\xampp\htdocs\joomla-templates\jsn_force_2/images/stories/virtuemart/product/397.jpg
+2017-06-26 04:17:03 ERROR vmError: Couldnt create thumb, file not found C:\xampp\htdocs\joomla-templates\jsn_force_2\images\stories\virtuemart\product\397.jpg
+2017-06-26 04:17:03 ERROR vmError: File not found C:\xampp\htdocs\joomla-templates\jsn_force_2/images/stories/virtuemart/product/28.jpg
+2017-06-26 04:17:03 ERROR vmError: Couldnt create thumb, file not found C:\xampp\htdocs\joomla-templates\jsn_force_2\images\stories\virtuemart\product\28.jpg
+2017-06-26 04:17:03 ERROR vmError: File not found C:\xampp\htdocs\joomla-templates\jsn_force_2/images/stories/virtuemart/product/57.jpg
+2017-06-26 04:17:03 ERROR vmError: Couldnt create thumb, file not found C:\xampp\htdocs\joomla-templates\jsn_force_2\images\stories\virtuemart\product\57.jpg
+2017-06-26 04:17:03 ERROR vmError: File not found C:\xampp\htdocs\joomla-templates\jsn_force_2/images/stories/virtuemart/product/424.jpg
+2017-06-26 04:17:03 ERROR vmError: Couldnt create thumb, file not found C:\xampp\htdocs\joomla-templates\jsn_force_2\images\stories\virtuemart\product\424.jpg
+2017-06-26 04:17:03 ERROR vmError: File not found C:\xampp\htdocs\joomla-templates\jsn_force_2/images/stories/virtuemart/product/151.jpg
+2017-06-26 04:17:03 ERROR vmError: Couldnt create thumb, file not found C:\xampp\htdocs\joomla-templates\jsn_force_2\images\stories\virtuemart\product\151.jpg
+2017-06-26 04:17:03 ERROR vmError: File not found C:\xampp\htdocs\joomla-templates\jsn_force_2/images/stories/virtuemart/product/3.jpg
+2017-06-26 04:17:03 ERROR vmError: Couldnt create thumb, file not found C:\xampp\htdocs\joomla-templates\jsn_force_2\images\stories\virtuemart\product\3.jpg
+2017-06-26 04:17:23 ERROR vmError: Warning, the Safe Path is &lt;b&gt;not accessible&lt;/b&gt; (does not exist or no permission). For safety reasons it is very important to create a folder in a path not accessible by an URL or unguessable name, create also a folder 'invoices' and 'keys' in it to store your sensitive data secure. Our suggested path is for your system C:\xampp\htdocs\joomla-templates\vmfiles\ &lt;br&gt;You can also use a complex folder name as 'password' for example C:\xampp\htdocs\joomla-templates\jsn_force_2/administrator/components/com_virtuemart\yUCVuXqQJkRN\ Use this link to the &lt;a href='http://localhost/joomla-templates/jsn_force_2/administrator/index.php?option=com_virtuemart&amp;view=config' &gt;config&lt;/a&gt;
+2017-06-26 04:18:19 ERROR vmError: File not found C:\xampp\htdocs\joomla-templates\jsn_force_2/images/stories/virtuemart/product/28.jpg
+2017-06-26 04:18:19 ERROR vmError: Couldnt create thumb, file not found C:\xampp\htdocs\joomla-templates\jsn_force_2\images\stories\virtuemart\product\28.jpg
+2017-06-26 04:18:19 ERROR vmError: File not found C:\xampp\htdocs\joomla-templates\jsn_force_2/images/stories/virtuemart/product/397.jpg
+2017-06-26 04:18:19 ERROR vmError: Couldnt create thumb, file not found C:\xampp\htdocs\joomla-templates\jsn_force_2\images\stories\virtuemart\product\397.jpg
+2017-06-26 04:18:19 ERROR vmError: File not found C:\xampp\htdocs\joomla-templates\jsn_force_2/images/stories/virtuemart/product/3.jpg
+2017-06-26 04:18:19 ERROR vmError: Couldnt create thumb, file not found C:\xampp\htdocs\joomla-templates\jsn_force_2\images\stories\virtuemart\product\3.jpg
+2017-06-26 04:18:19 ERROR vmError: File not found C:\xampp\htdocs\joomla-templates\jsn_force_2/images/stories/virtuemart/product/151.jpg
+2017-06-26 04:18:19 ERROR vmError: Couldnt create thumb, file not found C:\xampp\htdocs\joomla-templates\jsn_force_2\images\stories\virtuemart\product\151.jpg
+2017-06-26 04:18:19 ERROR vmError: File not found C:\xampp\htdocs\joomla-templates\jsn_force_2/images/stories/virtuemart/product/693.jpg
+2017-06-26 04:18:19 ERROR vmError: Couldnt create thumb, file not found C:\xampp\htdocs\joomla-templates\jsn_force_2\images\stories\virtuemart\product\693.jpg
+2017-06-26 04:18:19 ERROR vmError: File not found C:\xampp\htdocs\joomla-templates\jsn_force_2/images/stories/virtuemart/product/57.jpg
+2017-06-26 04:18:19 ERROR vmError: Couldnt create thumb, file not found C:\xampp\htdocs\joomla-templates\jsn_force_2\images\stories\virtuemart\product\57.jpg
+2017-06-26 04:19:39 ERROR vmError: File not found C:\xampp\htdocs\joomla-templates\jsn_force_2/images/stories/virtuemart/product/397.jpg
+2017-06-26 04:19:39 ERROR vmError: Couldnt create thumb, file not found C:\xampp\htdocs\joomla-templates\jsn_force_2\images\stories\virtuemart\product\397.jpg
+2017-06-26 04:19:39 ERROR vmError: File not found C:\xampp\htdocs\joomla-templates\jsn_force_2/images/stories/virtuemart/product/693.jpg
+2017-06-26 04:19:39 ERROR vmError: Couldnt create thumb, file not found C:\xampp\htdocs\joomla-templates\jsn_force_2\images\stories\virtuemart\product\693.jpg
+2017-06-26 04:19:39 ERROR vmError: File not found C:\xampp\htdocs\joomla-templates\jsn_force_2/images/stories/virtuemart/product/424.jpg
+2017-06-26 04:19:39 ERROR vmError: Couldnt create thumb, file not found C:\xampp\htdocs\joomla-templates\jsn_force_2\images\stories\virtuemart\product\424.jpg
+2017-06-26 04:19:39 ERROR vmError: File not found C:\xampp\htdocs\joomla-templates\jsn_force_2/images/stories/virtuemart/product/151.jpg
+2017-06-26 04:19:39 ERROR vmError: Couldnt create thumb, file not found C:\xampp\htdocs\joomla-templates\jsn_force_2\images\stories\virtuemart\product\151.jpg
+2017-06-26 04:19:39 ERROR vmError: File not found C:\xampp\htdocs\joomla-templates\jsn_force_2/images/stories/virtuemart/product/28.jpg
+2017-06-26 04:19:39 ERROR vmError: Couldnt create thumb, file not found C:\xampp\htdocs\joomla-templates\jsn_force_2\images\stories\virtuemart\product\28.jpg
+2017-06-26 04:19:39 ERROR vmError: File not found C:\xampp\htdocs\joomla-templates\jsn_force_2/images/stories/virtuemart/product/57.jpg
+2017-06-26 04:19:39 ERROR vmError: Couldnt create thumb, file not found C:\xampp\htdocs\joomla-templates\jsn_force_2\images\stories\virtuemart\product\57.jpg
+2017-06-26 04:52:23 ERROR vmError: Warning, the Safe Path is &lt;b&gt;not accessible&lt;/b&gt; (does not exist or no permission). For safety reasons it is very important to create a folder in a path not accessible by an URL or unguessable name, create also a folder 'invoices' and 'keys' in it to store your sensitive data secure. Our suggested path is for your system C:\xampp\htdocs\joomla-templates\vmfiles\ &lt;br&gt;You can also use a complex folder name as 'password' for example C:\xampp\htdocs\joomla-templates\jsn_force_2/administrator/components/com_virtuemart\SAotlLQ7Hkry\ Use this link to the &lt;a href='http://localhost/joomla-templates/jsn_force_2/administrator/index.php?option=com_virtuemart&amp;view=config' &gt;config&lt;/a&gt;
